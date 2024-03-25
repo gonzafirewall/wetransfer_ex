446cff3bdfbac283bbea635dc8f80df94851561e
Gonzalo Martinez <1536784+gonzafirewall@users.noreply.github.com>
2024-03-24 23:26:19 -0300

    enhancing test things

--- test/wetransfer_ex_test.exs
+++ test/wetransfer_ex_test.exs
@@ -2,20 +2,36 @@ defmodule WetransferExTest do
   use ExUnit.Case
   doctest WetransferEx
 
-  test "build req api" do
-    opts = {"1234", "555"}
-    req = WetransferEx.build_req(opts)
-    assert req.url == URI.parse("https://wetransfer.com/api/v4/transfers/1234/download")
-    assert req.body == nil
-    assert req.method == :post
-    assert req.options |> Map.has_key?(:json)
-    assert req.options.json |> Map.get(:intent) == "entire_transfer"
-    assert req.options.json |> Map.get(:security_hash) == opts |> elem(1)
+  defp build_req_options(context) do
+    new_context = context
+    |> Map.put(:opts, {"1234", "555"})
+    |> Map.put(:uri, "https://wetransfer.com/api/v4/transfers/1234/download")
+    {:ok, new_context}
   end
 
-  test "test invalids url" do
-    invalid_urls = ["asdasd", "www.google.com"]
-    Enum.each(invalid_urls, fn url ->
+  setup_all do
+    {:ok,
+    invalid_urls: ["asdasd", "www.google.com"],
+    }
+  end
+
+  describe "Test build req api" do
+    setup [:build_req_options]
+
+    test "build req api", %{opts: opts, uri: uri} do
+      req = WetransferEx.build_req(opts)
+      assert req.url == URI.parse(uri)
+      assert req.body == nil
+      assert req.method == :post
+      assert req.options |> Map.has_key?(:json)
+      assert req.options.json |> Map.get(:intent) == "entire_transfer"
+      assert req.options.json |> Map.get(:security_hash) == opts |> elem(1)
+    end
+
+  end
+
+  test "test invalids url", state do
+    Enum.each(state[:invalid_urls], fn url ->
       assert WetransferEx.download(url) == :error
     end)
   end
